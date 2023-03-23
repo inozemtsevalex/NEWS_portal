4185598baecfeedb506440cdd545d68a8f6ad87e
inozemtsevalex <sandars@gmail.com>
2023-03-23 21:58:01 +0300

    изменен внешний вид страницы /news/:
    Выводите новости в следующем виде — заголовок, дата публикации в формате день.месяц.год, затем первые 20 слов текста статьи. Можно вывести как списком, так и таблицей. Новости должны выводиться в порядке от более свежей к старой.
    Сверху страницы должно быть выведено количество всех новостей (используется фильтр news|length).
    По ссылке /news/<id новости> должна выводиться детальная информация о новости.
    Заголовок, дата публикации в формате день.месяц.год и полный текст статьи.

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,13 +1,18 @@
-
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 
 class NewsList(ListView):
     model = Post
-    ordering = 'createDate'
+    ordering = '-createDate'
     template_name = 'news_list.html'
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
 
 
 class NewsCopy(DetailView):
